a682caed5872eefdea2309c1e281a358f507c556
russellchristensen <rusifald@gmail.com>
2015-03-06 11:59:36 -0500

    started communication

--- src/app.js
+++ src/app.js
@@ -1,43 +1,96 @@
 var UI = require('ui');
-var ajax = require('ajax');
+//var ajax = require('ajax');
+
+var configuration = {
+  root: {
+    url: "192.168.1.1",
+    pages: [
+      {    
+        id: 1,
+        title: "Home",
+        controls:[
+          {
+            id: 1,
+            title: "System Status"
+          }
+        ]
+      },
+      {
+        id: 2,
+        title: "Living Room",
+        controls:[
+          { id: 1, title: "Play" },
+          { id: 2, title: "Pause" },
+          { id: 3, title: "FF" },
+          { id: 4, title: "RW" }
+        ]
+      }
+    ]
+  }
+};
 
 // Create a Card with title and subtitle
 var card = new UI.Card({
-  title:'Weather',
-  subtitle:'Fetching...'
+  title:'Home Control',
+  subtitle:'Loading...'
+  
+  
 });
 
 // Display the Card
 card.show();
+card.hide();
+var mainMenu = new UI.Menu({
+  sections: [{
+    title:'Pages',
+    items:configuration.root.pages
+  }]
+});
 
-// Construct URL
-var cityName = 'London';
-var URL = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+var currentMenu = configuration.root.pages;
 
-// Make the request
-ajax(
-  {
-    url: URL,
-    type: 'json'
-  },
-  function(data) {
-    // Success!
-    console.log("Successfully fetched weather data!");
-
-    // Extract data
-    var location = data.name;
-    var temperature = Math.round(data.main.temp - 273.15) + "C";
-
-    // Always upper-case first letter of description
-    var description = data.weather[0].description;
-    description = description.charAt(0).toUpperCase() + description.substring(1);
-
-    // Show to user
-    card.subtitle(location + ", " + temperature);
-    card.body(description);
-  },
-  function(error) {
-    // Failure!
-    console.log('Failed fetching weather data: ' + error);
+var menuEvent = function(event) {
+  console.log("event.item: " + JSON.stringify(event.item));
+  var selectedPage = event.item;
+  if (selectedPage.controls) {
+    if (selectedPage.controls.length > 1) {
+      currentMenu = selectedPage.controls;
+      var detailMenu = new UI.Menu({
+        sections: [{
+          title: selectedPage.title,
+          items: currentMenu
+        }]
+      });
+      detailMenu.on('select', menuEvent);
+      
+      detailMenu.show();
+    } else if (selectedPage.controls.length == 1) {
+      var onlyControl = selectedPage.controls[0];
+      
+      var detailCard = new UI.Card({
+        title: onlyControl.title,
+        body: 'Page Id: ' + onlyControl.id
+      });
+      detailCard.show();    
+    }
+  } else {
+    // Construct a dictionary
+    var dict = {
+      '0':42
+    };
+  
+    // Send a string to Pebble
+    Pebble.sendAppMessage(dict,
+      function(e) {
+        console.log('Send successful.');
+      },
+      function(e) {
+        console.log('Send failed!');
+      }
+    );
   }
-);
+};
+
+mainMenu.on('select', menuEvent);
+
+mainMenu.show();
